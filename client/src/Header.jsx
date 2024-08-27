2ecb3cc01351f93859ea35bf0ee8a13afc83defc
Bhavesh <bvuyyuru@umich.edu>
2024-08-27 15:03:38 -0400

    cleanup header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -24,29 +24,6 @@ export default function Header() {
           aribnc
         </span>
       </Link>
-      <div className="flex gap-2 border border-gray-400 rounded-full py-2 px-4 shadow-md shadow-gray-300">
-        <div>Anywhere</div>
-        <div className="border border-l border-gray-300"></div>
-        <div>Any Week</div>
-        <div className="border border-l border-gray-300"></div>
-        <div>Add guests</div>
-        <button className="bg-primary text-white p-1 rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-            />
-          </svg>
-        </button>
-      </div>
       <Link
         to={user ? "/account" : "/login"}
         className="flex gap-2 border border-gray-400 rounded-full py-2 px-4 shadow-md shadow-gray-300"
